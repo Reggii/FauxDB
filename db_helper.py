1a20bbabdcade3e842488a934ecd57af396ada8a
Rihhard Elm <elm.rihhard@gmail.com>
2022-12-09 21:10:06 +0200

    Update db_helper.py

--- db_helper.py
+++ db_helper.py
@@ -1,12 +1,13 @@
 import pymysql
 
-
+# Re-usable data base connection function
 def con():
     return pymysql.connect(
-        host="localhost", user="", password="", database=""
+        host="localhost", user="root", password="ctpn25EPT5TrLmeF", database="FauxDB"
     )
 
 
+# Create the necessary tables for storing user data and notes
 def create_db_tables():
     connection = con()
     with connection.cursor() as cursor:
@@ -30,6 +31,7 @@ def create_db_tables():
         cursor.execute(sql)
 
 
+# Check if the entered username and password match db records
 def query_user_from_db(username, password):
     connection = con()
     with connection:
@@ -43,11 +45,13 @@ def query_user_from_db(username, password):
             return False
 
 
+# Write username and password into the users table
 def write_user_to_db(username, password):
     connection = con()
     with connection:
         with connection.cursor() as cursor:
             sql = "INSERT INTO `users` (`username`, `password`) VALUES (%s, %s);"
+            # If the username already exists, we catch the Integrity Error and return False
             try:
                 cursor.execute(sql, (username, password))
                 connection.commit()
@@ -56,6 +60,8 @@ def write_user_to_db(username, password):
             return True
 
 
+# We write the note and the date to our notes table
+# and set the user_id to match the username
 def write_item(date, note, username):
     connection = con()
     cur_user = username
@@ -67,13 +73,17 @@ def write_item(date, note, username):
             cur_user_id = cursor.fetchone()
 
         with connection.cursor() as cursor:
-            sql = "INSERT INTO `notes` (`date`, `note`, `user_id`) " \
-                  "VALUES (%s, %s, %s);"
+            sql = (
+                "INSERT INTO `notes` (`date`, `note`, `user_id`) "
+                "VALUES (%s, %s, %s);"
+            )
             cursor.execute(sql, (date, note, cur_user_id))
             connection.commit()
-            return print('Note saved')
+            return print("Note saved")
 
 
+# We select the saved notes and dates relative to the user_id
+# for the current username and return all the rows
 def fetch_items(username):
     connection = con()
     cur_user = username
@@ -85,10 +95,7 @@ def fetch_items(username):
             cur_user_id = cursor.fetchone()
 
         with connection.cursor() as cursor:
-            sql = 'SELECT * FROM `notes` WHERE `user_id`=%s;'
+            sql = "SELECT * FROM `notes` WHERE `user_id`=%s;"
             cursor.execute(sql, cur_user_id)
             all_items = cursor.fetchall()
-            for row in all_items:
-                print(f'\nDate: {row[1]}\n'
-                      f'Note #{row[0]}: {row[2]}')
-
+            return all_items
