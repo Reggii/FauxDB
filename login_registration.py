cf7582dcd314428f9dc36267cd8106db6ff2aaea
Rihhard Elm <elm.rihhard@gmail.com>
2022-12-09 21:09:40 +0200

    Update login_registration.py

--- login_registration.py
+++ login_registration.py
@@ -2,8 +2,9 @@ import db_helper
 import write_fetch_notes
 
 
+# Create a UserData object with username, password and authorization status
 class UserData:
-    def __init__(self, username:str, password:str):
+    def __init__(self, username: str, password: str):
         self.username = username
         self.password = password
         self._authorized = False
@@ -17,31 +18,29 @@ class UserData:
         self._authorized = auth_feedback
 
 
+# Send a request to db_helper to check the username and password
+# confirm if successful
 def log_in(username, password):
     db_helper.create_db_tables()
     user = UserData(username, password)
     user.authorized = db_helper.query_user_from_db(username, password)
 
     if user.authorized:
-        print('\nLogin Successful!\n')
+        print("\nLogin Successful!\n")
         write_fetch_notes.query_user_for_action(username)
     if not user.authorized:
-        print(
-            '\nLogin Unsuccessful\n'
-            'Please restart and attempt again'
-        )
+        print("\nLogin Unsuccessful\n" "Please restart and attempt again")
 
 
+# Send a request to db_helper to write the username and password
+# confirm if successful
 def register(username, password):
     db_helper.create_db_tables()
     user = UserData(username, password)
     user.authorized = db_helper.write_user_to_db(username, password)
 
     if user.authorized:
-        print('\nRegistration Successful!')
+        print("\nRegistration Successful!")
         write_fetch_notes.query_user_for_action(username)
     if not user.authorized:
-        print(
-            'User already registered\n'
-            'Please restart the application and log-in'
-        )
+        print("User already registered\n" "Please restart the application and log-in")
